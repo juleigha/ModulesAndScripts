87bfaed465565f0541b5cb9bbc5a915a7b9118e8
juleigha <juleighaquist@gmail.com>
2022-06-07 14:18:00 -0400

    Update DynamicShadows.js

--- DynamicShadows.js
+++ DynamicShadows.js
@@ -1,8 +1,7 @@
 // This gives the effect of a light source on a page 
 // example at https://sqedg.com/Portfolio/dynamicShadows.html
 
-var scroll = window.pageYOffset;
-var staticLight = 200;
+var staticLight = 200; // light source distance from top
 changeLight();
 $(document).scroll(function(e) {
 	changeLight();
@@ -11,10 +10,8 @@ function changeLight() {
 	var shadowBlur;
 	var shadowRGB = "rgba(0, 0, 0, 0.8)";
 	var lightSource = window.pageYOffset + staticLight ;
-	var x = 0;
 	$(".lightSource-shadow").each(function() {
 		var shadowOffSet = (this.offsetTop - window.pageYOffset - staticLight) / 35;
 		$(this).css("box-shadow","0px "+(shadowOffSet)+"px 7px rgba(0, 0, 0, 0.50)" );
 	})
-	scroll = window.pageYOffset;
 }
