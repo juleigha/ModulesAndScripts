ec3da71eebe1c9216dbce939cc045481e4730b17
juleigha <juleighaquist@gmail.com>
2022-06-07 14:15:46 -0400

    Update DynamicShadows.js

--- DynamicShadows.js
+++ DynamicShadows.js
@@ -2,22 +2,19 @@
 // example at https://sqedg.com/Portfolio/dynamicShadows.html
 
 var scroll = window.pageYOffset;
-var shadowOffSetDiff = 0;
+var staticLight = 200;
+changeLight();
 $(document).scroll(function(e) {
+	changeLight();
+})
+function changeLight() {
 	var shadowBlur;
 	var shadowRGB = "rgba(0, 0, 0, 0.8)";
-	var lightSource = window.pageYOffset - 20;
-	shadowOffSetDiff -=1;
+	var lightSource = window.pageYOffset + staticLight ;
+	var x = 0;
 	$(".lightSource-shadow").each(function() {
-		var hgt = $(this).height();
-		var shadowOffSet = (this.offsetTop - (lightSource ) ) / 35;
-		if (shadowOffSet > -2 && shadowOffSet < 2){
-			$(this).css("box-shadow","0px "+(shadowOffSet)+"px 8px rgba(0, 0, 0, 0.50)" );
-		}
-		else {
-			$(this).css("box-shadow","0px "+(shadowOffSet)+"px 7px rgba(0, 0, 0, 0.51)" );
-			//rgb(23,21,0)
-		}
+		var shadowOffSet = (this.offsetTop - window.pageYOffset - staticLight) / 35;
+		$(this).css("box-shadow","0px "+(shadowOffSet)+"px 7px rgba(0, 0, 0, 0.50)" );
 	})
 	scroll = window.pageYOffset;
-})
+}
